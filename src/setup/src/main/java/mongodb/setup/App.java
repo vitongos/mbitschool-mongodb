60fac0ccf49c1acc1cf21e4ff9f0ef62a9213b4e
cloudera <cloudera@quickstart.cloudera>
2015-01-11 01:25:42 -0800

    Setup Project.

---
+++ src/setup/src/main/java/mongodb/setup/App.java
@@ -0,0 +1,14 @@
+package mongodb.setup;
+
+import static spark.Spark.get;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public static void main(String[] args) {
+        get("/hello", (req, res) -> "Setup complete!");
+    }
+}
