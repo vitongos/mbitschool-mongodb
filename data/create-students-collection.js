1886a9884f47661ef6f9915b29bd31a7116e9b52
Victor <vitongos@gmail.com>
2015-01-15 22:35:13 +0100

    Create students collection.

--- data/create-students-collection.js
+++ data/create-students-collection.js
@@ -1,5 +1,5 @@
-for (var i=1; i<=20000000; i++) {
-  var student = { student_id: i, scores: { mathematics: Math.floor(Math.random()*100), geography: Math.floor(Math.random()*100), english: Math.floor(Math.random()*100) }, "class" : Math.floor(Math.random()*10) };
+for (var i=1; i<=10000000; i++) {
+  var student = { student_id: i, scores: { mathematics: Math.floor(Math.random()*100), geography: Math.floor(Math.random()*100), english: Math.floor(Math.random()*100) }, "class" : Math.floor(Math.random()*10), previous : [ Math.floor(Math.random()*50) + 50, Math.floor(Math.random()*50) + 50, Math.floor(Math.random()*50) + 50, Math.floor(Math.random()*50) + 50 ] };
   db.students.insert(student);
   if (i % 10000 == 0) {
     print("Inserted: " + i);
