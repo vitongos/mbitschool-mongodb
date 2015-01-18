d09073abc9aa8bac3194b9eda5c8b5465c3348ca
Victor <vitongos@gmail.com>
2015-01-18 10:34:40 +0100

    Replica set configuration.

---
+++ data/replica-set-config.js
@@ -0,0 +1,20 @@
+rsconf = {
+	_id: "myReplicaSet",
+	members: [
+		{
+			_id: 0,
+			host: "localhost:30001"
+		},
+		{
+			_id: 1,
+			host: "localhost:30002"
+		},
+		{
+			_id: 2,
+			host: "localhost:30003"
+		}
+	]
+}
+rs.initiate(rsconf);
+rs.conf();
+
---
+++ deploy/replicaset.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mkdir -p /tmp/mongodb/member1 /tmp/mongodb/member2 /tmp/mongodb/member3
+cd /opt/mongodb
+
+bin/mongod --port 30001 --dbpath /tmp/mongodb/member1 --logpath /tmp/mongodb/member1/mongo.log --fork --replSet myReplicaSet --smallfiles --oplogSize 128
+bin/mongod --port 30002 --dbpath /tmp/mongodb/member2 --logpath /tmp/mongodb/member2/mongo.log --fork --replSet myReplicaSet --smallfiles --oplogSize 128
+bin/mongod --port 30003 --dbpath /tmp/mongodb/member3 --logpath /tmp/mongodb/member3/mongo.log --fork --replSet myReplicaSet --smallfiles --oplogSize 128
+
+cd ~/mongo-src/
+
+/opt/mongodb/bin/mongo --port 30001 < data/replica-set-config.js
